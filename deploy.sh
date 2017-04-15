77da4bb33632f7d3dea59d7b8b430c7dbef031ab
Phil Foster <ifoster@gmail.com>
2017-04-15 17:25:57 +1000

    Removing deploy stage from build process

--- deploy.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-dotnet restore
-dotnet lambda package
-BUILD_VERSION=`git log --pretty=format:'%h' -n 1`
-aws cloudformation package --template-file app_def.yml --output-template-file serverless-$BUILD_VERSION.yml --s3-bucket fitness-app-workouts-code
-#aws cloudformation deploy --template-file serverless-output.yml --stack-name Workouts --capabilities CAPABILITY_IAM
-
