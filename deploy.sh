5906fbc714dec1ccb1b044db20cfab2d2aebdf2a
Phil Foster <ifoster@gmail.com>
2017-04-15 13:14:06 +1000

    Setting up artifact containing cloudformation script

--- deploy.sh
+++ deploy.sh
@@ -2,6 +2,6 @@
 
 dotnet restore
 dotnet lambda package
-aws cloudformation package --template-file app_def.yml --output-template-file serverless-output.yml --s3-bucket fitness-code-dotnet-input
-aws cloudformation deploy --template-file serverless-output.yml --stack-name Workouts --capabilities CAPABILITY_IAM
+aws cloudformation package --template-file app_def.yml --output-template-file serverless-output.yml --s3-bucket fitness-app-workouts-code
+#aws cloudformation deploy --template-file serverless-output.yml --stack-name Workouts --capabilities CAPABILITY_IAM
 
